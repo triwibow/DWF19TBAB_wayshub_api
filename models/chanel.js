339bdf434df894a27c140bac86f78f32388ba049
triwibow <triw3707@gmail.com>
2020-12-05 00:02:17 +0700

    add Register API

---
+++ models/chanel.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Chanel extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Chanel.init({
+    email: DataTypes.STRING,
+    chanelName: DataTypes.STRING,
+    description: DataTypes.TEXT,
+    thumbnail: DataTypes.STRING,
+    photo: DataTypes.STRING,
+    password: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Chanel',
+  });
+  return Chanel;
+};
